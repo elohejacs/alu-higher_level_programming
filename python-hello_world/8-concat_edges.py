2e302771967e9fab0daabdc56a40729ed843e63e
elohejacs <elohejacs@gmail.com>
2024-10-29 05:42:49 -0700

    Task 8

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "object-oriented "
-str2 = "programming with "
-str3 = "Python"
-print(str1 + str2 + str3)
+#Python oop
+#task 8
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+print(str[39:67] + str[107:112] + str[:6])
