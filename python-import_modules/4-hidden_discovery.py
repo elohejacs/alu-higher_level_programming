a3c7041777e7e082771489ee7f18444019f6bfea
elohejacs <elohejacs@gmail.com>
2024-10-29 06:56:42 -0700

    Task 4

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import hidden_4
+
+
+if __name__ == "__main__":
+    # Get all names from the hidden_4 module
+    names = dir(hidden_4)
+
+    # Filter and sort names that do not start with '__'
+    filtered_names = [name for name in names if not name.startswith("__")]
+    filtered_names.sort()
+
+    # Print each name on a new line
+    for name in filtered_names:
+        print(name)
