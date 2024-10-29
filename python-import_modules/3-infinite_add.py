35e9a92065cbe2b3a1b33f127ce0238c9fab62c1
elohejacs <elohejacs@gmail.com>
2024-10-29 06:54:22 -0700

    Task 4

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,11 +2,14 @@
 import sys
 
 
+import sys
+
+
 if __name__ == "__main__":
     total = 0  # Initialize the total sum
 
     # Loop through the command-line arguments, starting from index 1
     for arg in sys.argv[1:]:
-        total += int(arg)  # Convert each argument to an integer and add to total
+        total += int(arg)  # Convert each argument to an integer
 
     print(total)  # Print the final total
