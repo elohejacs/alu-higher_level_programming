ff283099497bd763721860fdf807ea96f31a97dd
elohejacs <elohejacs@gmail.com>
2024-10-31 00:42:39 -0700

    Task 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
     count = 0
     try:
@@ -10,5 +11,3 @@ def safe_print_list(my_list=[], x=0):
         pass
     print("")
     return count
-
-
