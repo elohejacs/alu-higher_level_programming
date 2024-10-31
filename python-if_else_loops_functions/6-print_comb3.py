13e1110384718bcbbf9074031f917f63d72af0d5
elohejacs <elohejacs@gmail.com>
2024-10-31 00:17:00 -0700

    Task 6

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(10):
+for i in range(0, 10):
     for j in range(i + 1, 10):
-        print("{:02}{:02}".format(i, j), end=", " if not (i == 8 and j == 9) else "\n")
+        if i != 8 or j != 9:
+            print("{:02d}, ".format(i * 10 + j), end="")
+print("89")
