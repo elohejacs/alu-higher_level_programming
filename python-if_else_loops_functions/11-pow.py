414229c8ec4f4fb095d883b4e6d7c04f610c59c2
elohejacs <elohejacs@gmail.com>
2024-10-31 00:24:32 -0700

    Task 11

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
 def pow(a, b):
-    result = 1.0  # Use a float for more precise division
-    if b < 0:
-        for _ in range(-b):
-            result /= a
-    else:
-        for _ in range(b):
-            result *= a
-    return format(result, '.15g')  # Format to ensure precision
+    return a ** b
