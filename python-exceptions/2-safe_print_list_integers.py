8e94ac44446b67695933332544932c759c53963d
elohejacs <elohejacs@gmail.com>
2024-10-31 00:49:10 -0700

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -10,6 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             continue
         except IndexError:
-            break
+            break  # Stop if the index is out of range
     print("")
     return count
