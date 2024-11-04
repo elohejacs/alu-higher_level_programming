d6da9f63bff640f8d00a06a304a8ee988bc545f3
elohejacs <elohejacs@gmail.com>
2024-11-04 00:31:43 -0800

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    
     count = 0
     index = 0
-    
     while index < x:
         try:
             # Try to access the current index
