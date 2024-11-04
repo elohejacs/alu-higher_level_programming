c5d9e1b9a82e480dadc9ec7b1a2700d811d95f7c
elohejacs <elohejacs@gmail.com>
2024-11-04 00:14:11 -0800

    Task 2

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def safe_print_list_integers (my_list=[], x=0):
+def safe_print_list_integers(my_list=[], X=0):
     num = 0
-    for items in range(x):
+    for items in range(X):
         try:
             print("{:d}".format(my_list(items)), end='')
             num += 1
