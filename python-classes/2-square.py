29c6292b26d220919fd39c78481ec4a00499950b
elohejacs <elohejacs@gmail.com>
2024-11-03 23:01:37 -0800

    Task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -8,10 +8,8 @@ class Square:
     def __init__(self, size=0):
         """
         Initializes a new square with size validation.
-        
         Args:
             size (int): The size of the square. Defaults to 0.
-
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
