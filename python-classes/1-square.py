e2b97b799b48c6d340ba14b9045140c73d4bd674
elohejacs <elohejacs@gmail.com>
2024-11-03 22:47:42 -0800

    Task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class Square."""
+
+
+class Square:
+    """Represents a square."""
+    
+    def __init__(self, size):
+        """Initializes a new square"""
+        self.__size = size
