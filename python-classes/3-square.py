ac87df70d06adc30c12b67b97dd89334c383a6db
elohejacs <elohejacs@gmail.com>
2024-11-03 23:07:58 -0800

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""This module defines a class Square with size validation and area calculation."""
+"""defines a class Square with size validation and area."""
 
 
 class Square:
