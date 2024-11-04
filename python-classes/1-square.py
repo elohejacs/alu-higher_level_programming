fd30a68fee59214d6032dfd175c8ef0c932abf15
elohejacs <elohejacs@gmail.com>
2024-11-03 22:51:05 -0800

    Task 1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -3,8 +3,7 @@
 
 
 class Square:
-    """Represents a square."""
-    
+    """Represents a square."""    
     def __init__(self, size):
         """Initializes a new square"""
         self.__size = size
