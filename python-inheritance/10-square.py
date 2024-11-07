27e4e1ea74e3c4a6f03c6233b30cd2a38e1c514c
elohejacs <elohejacs@gmail.com>
2024-11-07 07:49:12 -0800

    Task 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,55 +1,16 @@
 #!/usr/bin/python3
-"""This module defines the Square class"""
-
-
-class BaseGeometry:
-
-    """BaseGeometry class with an area method that raises an exception."""
-    def area(self):
-        """Raises an exception if the area method is not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates that the value is a positive integer."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-
-    """Rectangle class that inherits from BaseGeometry."""
-    def __init__(self, width, height):
-        """Initialize the Rectangle with validated width and height."""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Return the area of the rectangle (width * height)."""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Return a string representation of the Rectangle."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+'''Module for Rectangle class.'''
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-
-    """Square class that inherits from Rectangle."""
+    '''A subclass representing a rectangle.'''
     def __init__(self, size):
-        """Initialize the Square with validated size."""
+        '''Constructor.'''
         self.integer_validator("size", size)
-        # Call the parent class constructor with size as both width and height
+        self.__size = size
         super().__init__(size, size)
 
     def area(self):
-        """Return the area of the square (size * size)."""
-        return self._Rectangle__width * self._Rectangle__height
-
-    def __str__(self):
-        """Return a string representation of the Square."""
-        return "angle".format(self._Rectangle__width, self._Rectangle__height)
+        '''Method for area of square.'''
+        return self.__size ** 2
