32651c02dea70d6ba35eed3c193a2e7a155ae8f6
elohejacs <elohejacs@gmail.com>
2024-11-07 05:45:23 -0800

    Task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""This module defines the Rectangle class that"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with an area method"""
+    
+    def area(self):
+        """Raises an exception if the area method"""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """Validates that the value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from BaseGeometry."""
+    
+    def __init__(self, width, height):
+        """Initialize the Rectangle with validated width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Return the area of the rectangle (width * height)."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Return a string representation of the Rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
