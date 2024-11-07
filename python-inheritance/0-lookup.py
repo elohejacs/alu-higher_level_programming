6a4e7003ed98ce53d85ebfa77cea10a3064eda54
elohejacs <elohejacs@gmail.com>
2024-11-07 04:15:34 -0800

    Task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a single function `lookup` that returns the list"""
+
+
 def lookup(obj):
     """Returns the list of available attributes and methods of an object."""
     return dir(obj)
