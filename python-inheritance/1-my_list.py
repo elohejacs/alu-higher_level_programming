eb28841343d1d19887b987ed641689c34f6021b1
elohejacs <elohejacs@gmail.com>
2024-11-07 04:34:56 -0800

    Task 1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for class"""
+
+
+class MyList(list):
+    """Inherits from list and includes a method to print a sorted version."""
+
+    def print_sorted(self):
+        """Prints the list in ascending order."""
+        print(sorted(self))
