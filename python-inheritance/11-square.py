ef4df78e43176bf5a63b5313263fed2fae002b34
elohejacs <elohejacs@gmail.com>
2024-11-07 06:50:39 -0800

    Task 1

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,11 +1,44 @@
 #!/usr/bin/python3
-"""This module defines the Square class"""
+"""
+This module defines the Square class, which inherits from Rectangle.
+"""
+
+class BaseGeometry:
+    """BaseGeometry class with methods integer validation."""
+
+    def area(self):
+        """Raises an exception if the area method is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates that value is a positive integer."""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
-from 9-rectangle import Rectangle
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits area calculation."""
+
+    def __init__(self, width, height):
+        """Initializes the Rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the string description of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
 
 class Square(Rectangle):
-    """Square class that inherits from Rectangle and implements specific behavior."""
+    """Square class that inheri specific behavior."""
 
     def __init__(self, size):
         """Initializes the Square with size."""
