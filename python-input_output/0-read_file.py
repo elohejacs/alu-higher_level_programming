7729c17400c5463fd09efc8e6acd53fc5a3fac84
elohejacs <elohejacs@gmail.com>
2024-11-10 23:53:56 -0800

    Task 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and rints it to stdout.
+    Args:
+        filename (str): The name of the file to be read.
+        """
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read(), end="")
