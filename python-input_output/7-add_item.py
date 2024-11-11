302a615484776f18bdedd436e3d18969ba32a7f8
elohejacs <elohejacs@gmail.com>
2024-11-11 00:40:00 -0800

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Task 7"""
 
+
 import sys
 
 
@@ -9,11 +10,19 @@ from 6_load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
-try:
-    items = load_from_json_file(filename)
-except FileNotFoundError:
-    items = []
+def main():
+    """
+    Adds all command-line arguments to a Python list.
+    The list is loaded from 'add_item.json' if it exists 
+    The updated list is saved back to the file.
+    """
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
 
-items.extend(sys.argv[1:])
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
 
-save_to_json_file(items, filename)
+if __name__ == "__main__":
+    main()
