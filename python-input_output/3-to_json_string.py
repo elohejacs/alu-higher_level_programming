521593901b033179642ae11dd0ed6c905f1624ae
elohejacs <elohejacs@gmail.com>
2024-11-11 00:14:57 -0800

    Task 3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Task 3"""
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Returns the JSON representation of an object (string).
+    Args:
+        my_obj (any): The object to be serialized.
+    Returns:
+        str: The JSON representation of the object.
+    """
+    return json.dumps(my_obj)
