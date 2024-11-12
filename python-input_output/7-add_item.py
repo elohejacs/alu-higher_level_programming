5623e29b15c546313ff1007a3c0cccb53c4a86f2
elohejacs <elohejacs@gmail.com>
2024-11-12 05:25:07 -0800

    Task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,8 @@ import sys
 from 5_save_to_json_file import save_to_json_file
 from 6_load_from_json_file import load_from_json_file
 
+"""TAsk 7 import sy and other files"""
+
 filename = "add_item.json"
 
 def main():
