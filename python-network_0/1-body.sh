dfe43a5035ecad51b46ca3e93e7b1920d65d5b28
Jacobel <148277388+elohejacs@users.noreply.github.com>
2024-11-20 10:32:32 +0200

    Task 0

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to the URL and displays the response body if status is 200
-curl -sL -o /dev/null -w "%{http_code}" "$1" | grep -q "200" && curl -s "$1"
+curl -s "$1" | wc -c
