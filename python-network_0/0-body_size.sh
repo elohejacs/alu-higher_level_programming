61bace9d50bab0d5ec60ea21939e271115af7841
Jacobel <148277388+elohejacs@users.noreply.github.com>
2024-11-21 11:34:49 +0200

    Task 0

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a request to a URL and displays the size of the response body in bytes
-curl -s "$1" -o /dev/null -w "%{size_download}\n"
+# Sends a GET request to a URL and displays the size of the response body in bytes
+curl -s "$1" | wc -c
