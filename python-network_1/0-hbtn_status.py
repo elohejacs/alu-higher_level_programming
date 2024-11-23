4fb4d2ae098c21f380830185e336084c579d8e80
Jacobel <148277388+elohejacs@users.noreply.github.com>
2024-11-23 10:57:02 +0200

    Task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
-"""https://alu-intranet.hbtn.io/status using urllib."""
+"""
+Fetches the status from https://alu-intranet.hbtn.io/status.
+The script prints the following details about the response:
+    - type: The type of the response content
+    - content: The raw content of the response
+    - utf8 content: The decoded content as a UTF-8 string
+"""
+
 import urllib.request
+"""using another  fnct"""
+
 # Fetching the URL content using urllib
 with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
     body = response.read()
+
 # Printing the response details
 print("Body response:")
 print(f"    - type: {type(body)}")
