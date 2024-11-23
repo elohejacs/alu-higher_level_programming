e8c7fca9bb1d22b1f1e38c43a2259a5126335606
elohejacs <elohe.jacs@gmail.com>
2024-11-23 07:20:24 +0000

    Task 3

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Display all HTTP methods the server of a given URL will accept.
-curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
+# Script to display all HTTP methods the server accepts
+curl -sX OPTIONS "$1" -I | grep "Allow:" | cut -d ':' -f 2 | sed 's/^ *//; s/ *$//'
