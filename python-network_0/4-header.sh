157ea6a03f7ce032eb07699b1aaea222423d122f
Jacobel <148277388+elohejacs@users.noreply.github.com>
2024-11-23 09:42:22 +0200

    Task 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Send a GET request to a given URL with a header variable.
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to the provided URL with a custom header
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
