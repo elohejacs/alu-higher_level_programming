a5978e6d4ce31bd2161e06a47e57bbd181514ae6
Jacobel <148277388+elohejacs@users.noreply.github.com>
2025-01-27 11:25:21 +0200

    Task 5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
+#!/usr/bin/node
+const arg = process.argv[2]; // Get the first argument
+const num = parseInt(arg); // Convert to integer
 
+if (isNaN(num)) {
+  console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
+}
