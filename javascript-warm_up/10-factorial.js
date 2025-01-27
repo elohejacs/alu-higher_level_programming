50a33a50dea83e528421c715d39e752462dd7df5
Jacobel <148277388+elohejacs@users.noreply.github.com>
2025-01-27 11:28:08 +0200

    Task 10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,1 +1,11 @@
+#!/usr/bin/node
+const arg = parseInt(process.argv[2]);
 
+function factorial(n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(arg));
