27e6271b2f3ec7f854f89d6fb482e7536b44ac36
elohejacs <elohe.jacs@gmail.com>
2025-01-29 05:01:40 -0800

    Tasks

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
 
-const firstArg = parseInt(process.argv[2]);
-const secondArg = parseInt(process.argv[3]);
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
 
-console.log(add(firstArg, secondArg));
+console.log(add(num1, num2));
