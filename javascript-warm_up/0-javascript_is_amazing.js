d3234f972e605feec2982caf112e425201819358
Jacobel <148277388+elohejacs@users.noreply.github.com>
2025-01-29 12:23:50 +0200

    Update 0-javascript_is_amazing.js

--- javascript-warm_up/0-javascript_is_amazing.js
+++ javascript-warm_up/0-javascript_is_amazing.js
@@ -1,3 +1,4 @@
-#!/usr/bin/node
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+# Create the file again to be sure
+echo '#!/usr/bin/node
+const myVar = '"'"'JavaScript is amazing'"'"';
+console.log(myVar);' > 0-javascript_is_amazing.js
