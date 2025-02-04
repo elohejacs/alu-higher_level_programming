5b5b8d08b5fef81b9db0a47f769723e8e4903437
elohejacs <elohe.jacs@gmail.com>
2025-02-04 06:11:07 -0800

    Task 6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,22 @@
+#!/usr/bin/node
 
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const tasks = JSON.parse(body);
+    const completedTasks = {};
+
+    tasks.forEach(task => {
+      if (task.completed) {
+        completedTasks[task.userId] = (completedTasks[task.userId] || 0) + 1;
+      }
+    });
+
+    console.log(completedTasks);
+  }
+});
