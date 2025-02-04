9d92360a37f0baa1a976656972c76fa16d5241c6
elohejacs <elohe.jacs@gmail.com>
2025-02-04 05:49:07 -0800

    Task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,17 @@
+#!/usr/bin/node
 
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const films = JSON.parse(body).results;
+    const count = films.filter(film =>
+      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    ).length;
+    console.log(count);
+  }
+});
